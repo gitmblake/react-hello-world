ec2b4e2181410db4b90dc25a9e64913804ed2893
Michael Blake <Michael.Blake@health.qld.gov.au>
2016-09-22 16:49:05 +1000

    changed index

--- app/index.js
+++ app/index.js
@@ -1,17 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-class HelloWorld extends React.Component {
-    render() {
-        return (
+
+var HelloWorld = React.createClass({
+    render: function() {
+    return (
             <div> Hello {this.props.name}</div>
         )
     }
-}
-    
-
-
-
+})
 
 ReactDOM.render(
     <HelloWorld name="Michael" />,
